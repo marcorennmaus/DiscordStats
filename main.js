a3afd736d9d7604ff0055691b5948c120925bdda
marcorennmaus <17mh2000@gmail.com>
2017-06-17 21:28:26 +0200

    v0.1.2
    Changes:
    - Increased size of Server column from 16 to 32

--- main.js
+++ main.js
@@ -32,14 +32,14 @@ http.createServer(function(request, response) {
     body = Buffer.concat(body).toString();
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
-    var responselist = "Pos | Server           | Messages\n"
+    var responselist = "Pos | Server                           | Messages\n"
     for(i = 0;i < numbers.length;i++){
       //Filling up variables with some spaces
       var posnumber = i + 1
       var posnumber = posnumber + "   "
-      var servernames = numbers[i].name + "                " + "."
+      var servernames = numbers[i].name + "                                " + "."
       //Cutting variables to fit nicely into that table
-      var servernames = servernames.substr(0, 16)
+      var servernames = servernames.substr(0, 32)
       var posnumber = posnumber.substr(0, 3)
 
       responselist = responselist + posnumber + " | " + servernames + " | " + numbers[i].value + "\n"
