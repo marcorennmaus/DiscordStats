9549582cd2d47e632472ee4f9d9b59ad3bba7f90
marcorennmaus <17mh2000@gmail.com>
2017-06-19 19:17:18 +0200

    v0.4.2
    Added lookup for individual users
    
    Code cleanup and movement to a Bot-Account soon

--- main.js
+++ main.js
@@ -229,6 +229,30 @@ client.on('message', message => {
     }
     message.channel.send("```" + responselist + "```")
   }
+
+  if (message.content.startsWith("hey marco give me the rank of someone") && message.author.username === "marco_rennmaus | Rennmoose"){
+    var msgargs = message.content.split(" ")
+    var searchauthor = msgargs[8]
+    var schAuthorHasBeenFound = false;
+
+
+    for(i = 0;i < numbersauth.length;i++){
+      if(searchauthor === numbersauth[i].usrid){
+        var schAuthorHasBeenFound = true;
+        var authorinarray = i;
+        //console.log("author has been found")
+      }
+    }
+
+    if(schAuthorHasBeenFound){
+      var rankauth = authorinarray + 1
+      message.channel.send("**" + numbersauth[authorinarray].name + "** is currently on Place " + rankauth + " with a total of " + numbersauth[authorinarray].value + " messages")
+    }
+
+    if(schAuthorHasBeenFound != true){
+      message.channel.send("User was not found")
+    }
+  }
 });
 
 //Bot Token
